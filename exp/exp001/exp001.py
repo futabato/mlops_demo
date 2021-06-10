7f941fa1a52f1b28a17e8d112b41d03e49de2249
futabato <01futabato10@gmail.com>
2021-06-11 01:38:50 +0900

    Add the demo code for experiment management combining hydra and mlflow

--- exp/exp001/exp001.py
+++ exp/exp001/exp001.py
@@ -7,6 +7,8 @@ from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME
 import mlflow.keras
 import hydra
 
+import os
+
 def create_model(loss, optimizer, metrics):
     model = keras.models.Sequential()
     model.add(keras.layers.Flatten(input_shape=[28, 28]))
@@ -21,19 +23,15 @@ def train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, 
     test_loss, test_accuracy = model.evaluate(X_test, y_test)
     return test_loss, test_accuracy
 
-
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg):
+    # hydraを使おうとするとcurrent directoryが変更されてしまうので、以下の設定が必要。
+    mlflow.set_tracking_uri("file://" + hydra.utils.get_original_cwd() + "/mlruns")
+    
     # 実験番号のロード
     experiment_name = cfg.exp.number
     mlflow.set_experiment(experiment_name)
-
-    remote_server_uri = 'http://127.0.0.1:5000'
-    mlflow.set_tracking_uri(remote_server_uri) 
-
-    elastic_ip_uri = 'http://107.22.178.5:5000'
-    #mlflow.set_tracking_uri(elastic_ip_uri) 
-
+    
     # dataの用意。abciのパスをyamlに書くことになりそう。
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
     X_train, X_valid = X_train[5000:], X_train[:5000]
@@ -41,38 +39,37 @@ def main(cfg):
     X_train, X_valid = X_train /255, X_valid /255
 
     # mlflowに記録を始めさせる
-    with mlflow.start_run(run_name=experiment_name):
+    mlflow.start_run(run_name=experiment_name)
+    mlflow.keras.autolog()
+    
+    # configからのロード
+    loss = cfg.training.loss
+    optimizer = cfg.training.optimizer
+    metrics = cfg.training.metrics
+    epoch = cfg.training.epoch
+    batch_size = cfg.training.batch_size
 
-        # configからのロード
-        loss = cfg.training.loss
-        optimizer = cfg.training.optimizer
-        metrics = cfg.training.metrics
-        epoch = cfg.training.epoch
-        batch_size = cfg.training.batch_size
+    model = create_model(loss, optimizer, metrics)
+    test_loss, test_accuracy = train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, y_test)
+    
+    # mlflow周りの保存
+    mlflow.log_param("loss", loss)
+    mlflow.log_param("optimizer", optimizer)
+    mlflow.log_param("metrics", metrics)
+    mlflow.log_param("epoch", epoch)
+    mlflow.log_param("batch_size", batch_size)
 
-        model = create_model(loss, optimizer, metrics)
-        
-        mlflow.keras.autolog()
-        test_loss, test_accuracy = train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, y_test)
-        
-        #model.fit(X_train, y_train, epochs=epoch, batch_size=batch_size, validation_data=(X_valid, y_valid))
-        
-        # mlflow周りの保存
-        mlflow.log_param("loss", loss)
-        mlflow.log_param("optimizer", optimizer)
-        mlflow.log_param("metrics", metrics)
-        mlflow.log_param("epoch", epoch)
-        mlflow.log_param("batch_size", batch_size)
+    mlflow.log_metrics({'loss': test_loss})
+    mlflow.log_metrics({'accuracy': test_accuracy})    
+    
+    mlflow.keras.log_model(model, cfg.exp.number)
+    
+    # hydraのyamlの保存
+    mlflow.log_artifact('.hydra/config.yaml')
+    mlflow.log_artifact('.hydra/hydra.yaml')
+    mlflow.log_artifact('.hydra/overrides.yaml')
 
-        mlflow.log_metrics({'loss': test_loss})
-        mlflow.log_metrics({'accuracy': test_accuracy})    
-        
-        mlflow.keras.log_model(model, cfg.exp.number)
-        
-        # hydraのyamlの保存
-        mlflow.log_artifact('.hydra/config.yaml')
-        mlflow.log_artifact('.hydra/hydra.yaml')
-        mlflow.log_artifact('.hydra/overrides.yaml')
+    mlflow.end_run()
 
 if __name__ == "__main__":
     main()
