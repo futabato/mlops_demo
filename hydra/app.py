61535f242d46f652072a614b46f19145c4970af8
futabato <01futabato10@gmail.com>
2021-06-16 18:26:19 +0900

    Fix redundant expression

--- hydra/app.py
+++ hydra/app.py
@@ -4,9 +4,9 @@ import hydra
 def app(cfg) -> None:
     config = cfg
     print(config)
-    batch_size = cfg.model.batch_size
+    batch_size = cfg.model.hyperparam.batch_size
     print(batch_size)
-    data = cfg.data.img
+    data = cfg.data.train.img
     print(data)
 if __name__ == "__main__":
     app()
