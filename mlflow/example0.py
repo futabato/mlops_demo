49ac9e7332ec6e403e263b3991afed367cb86b7f
futabato <01futabato10@gmail.com>
2021-06-17 16:49:48 +0900

    Fix minor change

--- mlflow/example0.py
+++ mlflow/example0.py
@@ -1,17 +1,15 @@
-import os
 import mlflow
 
-mlflow.start_run()
+with mlflow.start_run():
 
-mlflow.log_param("param1", 5)
+    mlflow.log_param("param1", 5)
 
-mlflow.log_metric("foo", 1)
-mlflow.log_metric("foo", 2)
-mlflow.log_metric("foo", 3)
+    mlflow.log_metric("foo", 1)
+    mlflow.log_metric("foo", 2)
+    mlflow.log_metric("foo", 3)
 
-with open("output.txt", "a") as f:
-    f.write("Hello")
+    with open("output.txt", "a") as f:
+        f.write("Hello")
 
-mlflow.log_artifact("output.txt")
+    mlflow.log_artifact("output.txt")
 
-mlflow.end_run()
