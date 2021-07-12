e915d8bf3b89a8b0ce76d46ab30373efe5b00285
futabato <01futabato10@gmail.com>
2021-07-12 20:18:06 +0900

    :recycle: Refactor about yaml file reference.

--- hydra/example1.py
+++ hydra/example1.py
@@ -1,12 +1,20 @@
 import hydra
 
-@hydra.main(config_path="conf", config_name="config")
-def app(cfg) -> None:
+@hydra.main(config_path="conf", config_name="config.yaml")
+def main(cfg):
     config = cfg
-    print(config)
+
+    image_path = cfg.data.train.image_path
+    label_path = cfg.data.train.label_path
+    print(f'image_path: {image_path}')
+    print(f'label_path: {label_path}')
+
+    dropout = cfg.model.hyperparam.dropout
     batch_size = cfg.model.hyperparam.batch_size
-    print(batch_size)
-    data = cfg.data.train.img
-    print(data)
+    epochs = cfg.model.hyperparam.epochs
+    print(f'batch_size: {batch_size}')
+    print(f'dropout: {dropout}')
+    print(f'epochs: {epochs}')
+
 if __name__ == "__main__":
-    app()
+    main()
