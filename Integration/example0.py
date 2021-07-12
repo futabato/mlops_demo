305b5240f3d8816f4718114fcefad8a6ab0dea05
futabato <01futabato10@gmail.com>
2021-07-13 00:33:19 +0900

    :sparkles: Implement an experimental code that integrates MLflow and Hydra.

--- Integration/example0.py
+++ Integration/example0.py
@@ -3,16 +3,15 @@ from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 
 import mlflow
-from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME
 import hydra
 
 import os
 
-def create_model(loss, optimizer, metrics):
+def create_model(loss, optimizer, metrics, maps):
     model = keras.models.Sequential()
     model.add(keras.layers.Flatten(input_shape=[28, 28]))
-    model.add(keras.layers.Dense(300, activation='relu'))
-    model.add(keras.layers.Dense(300, activation='relu'))
+    model.add(keras.layers.Dense(maps, activation='relu'))
+    model.add(keras.layers.Dense(maps, activation='relu'))
     model.add(keras.layers.Dense(10, activation='softmax'))
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     return model
@@ -24,48 +23,43 @@ def train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, 
 
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg):
-    # hydraを使おうとするとcurrent directoryが変更されてしまうので、以下の設定が必要。
+    # hydraを使おうとするとworking directoryが変更されてしまうので、以下の設定が必要。
     mlflow.set_tracking_uri("file://" + hydra.utils.get_original_cwd() + "/mlruns")
-    
-    # 実験番号のロード
-    experiment_name = cfg.exp.number
+
+    # Experimetの指定
+    experiment_name = cfg.setting.experiment
     mlflow.set_experiment(experiment_name)
-    
-    # dataの用意。abciのパスをyamlに書くことになりそう。
+
+    # 実験番号の指定
+    run_name = cfg.setting.run_name
+
+    # Dataの用意
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
     X_train, X_valid = X_train[5000:], X_train[:5000]
     y_train, y_valid = y_train[5000:], y_train[:5000]
     X_train, X_valid = X_train /255, X_valid /255
 
     # mlflowに記録を始めさせる
-    mlflow.start_run(run_name=experiment_name)
-    mlflow.tensorflow.autolog()
-    
-    # configからのロード
-    loss = cfg.model.training.loss
-    optimizer = cfg.model.training.optimizer
-    metrics = cfg.model.training.metrics
-    epoch = cfg.model.training.epoch
-    batch_size = cfg.model.training.batch_size
+    with mlflow.start_run(run_name=run_name):
+        # Auto Logging
+        mlflow.tensorflow.autolog()
 
-    model = create_model(loss, optimizer, metrics)
-    test_loss, test_accuracy = train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, y_test)
-    
-    # mlflow周りの保存
-    mlflow.log_param("loss", loss)
-    mlflow.log_param("optimizer", optimizer)
-    mlflow.log_param("metrics", metrics)
-    mlflow.log_param("epoch", epoch)
-    mlflow.log_param("batch_size", batch_size)
+        # configからのロード
+        loss = cfg.model.training.loss
+        optimizer = cfg.model.training.optimizer
+        metrics = cfg.model.training.metrics
+        epoch = cfg.model.training.epoch
+        batch_size = cfg.model.training.batch_size
 
-    mlflow.log_metrics({'loss': test_loss})
-    mlflow.log_metrics({'accuracy': test_accuracy})
-    
-    mlflow.log_artifact('.hydra/config.yaml')
-    mlflow.log_artifact('.hydra/hydra.yaml')
-    mlflow.log_artifact('.hydra/overrides.yaml')
+        maps = cfg.model.model.maps
 
-    mlflow.end_run()
+        model = create_model(loss, optimizer, metrics, maps)
+        test_loss, test_accuracy = train(model, epoch, batch_size, X_train, y_train, X_valid, y_valid, X_test, y_test)
+
+        # working directoryは`./output/日付/時間/に変更されているためこのパスの指定で良い`
+        mlflow.log_artifact('.hydra/config.yaml')
+        mlflow.log_artifact('.hydra/hydra.yaml')
+        mlflow.log_artifact('.hydra/overrides.yaml')
 
 if __name__ == "__main__":
     main()
